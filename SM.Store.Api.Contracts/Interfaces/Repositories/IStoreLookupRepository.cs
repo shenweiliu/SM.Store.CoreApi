ca6d6445b450d7df1005801c939a9e9a27f4ebe9
shenweiliu <gliuxs2@gmail.com>
2025-04-12 18:25:49 -0400

    Delete SM.Store.Api.Contracts directory

--- SM.Store.Api.Contracts/Interfaces/Repositories/IStoreLookupRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace SM.Store.Api.Contracts.Interfaces
-{
-    public interface IStoreLookupRepository<TEntity> : IGenericRepository<TEntity>  where TEntity : class
-    {        
-    }
-}
